099584e68c838eb2b7afd70e8d663d8275c34b58
Philipe Fatio <me@phili.pe>
2015-08-21 22:40:25 +0200

    Use Module#prepend instead of aliasing

--- lib/i18n/debug.rb
+++ lib/i18n/debug.rb
@@ -22,15 +22,8 @@ module I18n
     end
 
     module Hook
-      def self.included(klass)
-        klass.class_eval do
-          alias_method :lookup_without_debug, :lookup
-          alias_method :lookup, :lookup_with_debug
-        end
-      end
-
-      def lookup_with_debug(*args)
-        lookup_without_debug(*args).tap do |result|
+      def lookup(*args)
+        super.tap do |result|
           options = args.last.is_a?(Hash) ? args.pop : {}
           separator = options[:separator] || I18n.default_separator
           key = I18n.normalize_keys(*args, separator).join(separator)
@@ -40,5 +33,5 @@ module I18n
     end
   end
 
-  Backend::Simple.include(Debug::Hook)
+  Backend::Simple.prepend(Debug::Hook)
 end
